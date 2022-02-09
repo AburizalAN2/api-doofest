3aa22eeb2330fab0009d5f40ad4d1d009fbdd819
AburizalAN <56324469+AburizalAN@users.noreply.github.com>
2022-02-09 22:39:06 +0700

    add banner-doofest routes

--- index.js
+++ index.js
@@ -4,10 +4,32 @@ const path = require('path');
 const app = express();
 
 const bannerRoutes = require('./src/routes/banner-doofest');
+const productsRoutes = require('./src/routes/products');
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', `
+    http://localhost:3001, 
+    http://localhost:3000,
+    https://dashboard.doogether.tech,
+    https://dashboard.doogether.id
+  `);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+// {base_url}/api/banner-doofest
 app.use('/api', bannerRoutes);
+
+// {base_url}/api/products
+// app.use('/api', productsRoutes);
+
+// {base_url}/api/flashsale
+// app.use('/api', productsRoutes);
+
+
 
 app.listen('4000', () => {
   console.log('connected to port 4000')
