08ec8c2f16dca584ce6e1daaa3b8229459c4e606
AburizalAN <56324469+AburizalAN@users.noreply.github.com>
2022-02-11 01:45:56 +0700

    add route flashsale

--- src/routes/flashsale.js
+++ src/routes/flashsale.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const controller = require('../controllers/flashsale');
 
 router.get('/flashsale', controller.getFlashSaleList);
+router.post('/flashsale', controller.postFlashSale);
+router.patch('/flashsale', controller.updateFlashSale);
+router.delete('/flashsale', controller.deleteFlashSale);
 
 module.exports = router;
